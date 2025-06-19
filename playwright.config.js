deee4816d0a731abd2bc337ace90b89625539fba
Artur Oliveira <arturolivv@gmail.com>
2025-06-18 21:06:52 -0300

    Colocando o 'headless: false para tentar corrigir uma possível incoerencia

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   ],
   use: {
     trace: 'on',
-    headless: true,
+    headless: false,
     viewport: { width: 1280, height: 720 },
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
